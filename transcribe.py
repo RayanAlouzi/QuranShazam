50f2a1305f4c0b3e5b5a735e8776995bccfc5867
Rayan Alouzi <rayanbassam3@gmail.com>
2023-05-19 01:11:49 -0600

    transcribing properly

--- transcribe.py
+++ transcribe.py
@@ -1,13 +1,13 @@
-from os import path
 from pydub import AudioSegment
-
 import whisper 
 
+# start the model
 model = whisper.load_model("base")
 
+# turn mp3 to wav file
 sound = AudioSegment.from_mp3("transcript.mp3")
 sound.export("transcript.wav", format="wav")
 
-result = model.transcribe('transcript.wav',fp16 = False)
-
-print(result)
+# get result
+result = model.transcribe('transcript.wav', language="ar", fp16 = False)
+print(result['text'])
