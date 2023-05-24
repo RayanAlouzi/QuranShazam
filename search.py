9bfff72a1fc5cb806ccba4458b67ded2fd779393
Rayan Alouzi <rayanbassam3@gmail.com>
2023-05-23 22:43:56 -0600

    comments of what to do next

---
+++ search.py
@@ -0,0 +1,10 @@
+# searches quran-simple.txt for verse from voice note provided
+
+
+# open the file and retreive text wanted 
+
+
+# write search algorithm 
+
+
+# return result if found or if not found
---
+++ voicerecorder.py
@@ -0,0 +1,9 @@
+# take in voice and turn to mp3 file called transcript.mp3
+
+
+
+
+# voice record for certain amount of time 
+
+
+# turn to mp3
