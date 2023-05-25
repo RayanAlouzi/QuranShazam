fa540940ae3925f2202c715736dca9a0ca78abf6
Rayan Alouzi <rayanbassam3@gmail.com>
2023-05-25 13:22:59 -0600

    records properly
    doesnt handle excpetions but records voice notes into transcript.wav

--- voicerecorder.py
+++ voicerecorder.py
@@ -1,26 +1,26 @@
 # take in voice and turn to wav file called transcript.wav
 import sounddevice as sd
 from scipy.io.wavfile import write
-import wavio as wv
+
+# voice record for a certain amount of time
+def recordingTime():
+    time = input("How long would you like to record for: ")
     
-# voice record for certain amount of time 
-def recordingTime(): 
-    time = input("How long would you like to record for")
-    return time
+    return float(time)
 
 # actual recording
 def record(time):
     frequency = 44100
     duration = time
 
-    recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=2) # Start recorder with frequency and duration
+    recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=2)
     print("Recording...")
     sd.wait()
 
     write("transcript.wav", frequency, recording)
 
 
+# test
 if __name__ == "__main__":
-
     time = recordingTime()
     record(time)
