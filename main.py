a55e4dbd55ac327ece6e3e747cc803a7d85af857
Rayan Alouzi <rayanbassam3@gmail.com>
2023-05-25 02:05:10 -0600

    small changes

---
+++ main.py
@@ -0,0 +1,5 @@
+import voicerecorder as vr
+import transcribe
+
+def main(): 
+    return
--- transcribe.py
+++ transcribe.py
@@ -1,13 +1,17 @@
 from pydub import AudioSegment
 import whisper 
 
-# start the model
-model = whisper.load_model("base")
 
-# turn mp3 to wav file
-sound = AudioSegment.from_mp3("transcript.mp3")
-sound.export("transcript.wav", format="wav")
+class transcribe:
 
-# get result
-result = model.transcribe('transcript.wav', language="ar", fp16 = False)
-print(result['text'])
+    def transcribeAudio():
+        # start the model
+        model = whisper.load_model("base")
+
+        # turn mp3 to wav file
+        sound = AudioSegment.from_mp3("transcript.mp3")
+        sound.export("transcript.wav", format="wav")
+
+        # get result
+        result = model.transcribe('transcript.wav', language="ar", fp16 = False)
+        print(result['text'])
