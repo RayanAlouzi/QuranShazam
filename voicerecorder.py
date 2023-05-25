f7a32afdd289a5c25321b662cb8b01f0e01241b5
Rayan Alouzi <rayanbassam3@gmail.com>
2023-05-25 02:05:04 -0600

    made a voice recorder (not working yet)

--- voicerecorder.py
+++ voicerecorder.py
@@ -1,9 +1,26 @@
-# take in voice and turn to mp3 file called transcript.mp3
-
-
-
-
+# take in voice and turn to wav file called transcript.wav
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+    
 # voice record for certain amount of time 
+def recordingTime(): 
+    time = input("How long would you like to record for")
+    return time
+
+# actual recording
+def record(time):
+    frequency = 44100
+    duration = time
+
+    recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=2) # Start recorder with frequency and duration
+    print("Recording...")
+    sd.wait()
+
+    write("transcript.wav", frequency, recording)
 
 
-# turn to mp3
+if __name__ == "__main__":
+
+    time = recordingTime()
+    record(time)
